c0cb4fb270f20f7f330709b690cb882520844075
ThatOneGin <pedrodragoncrystal@gmail.com>
2024-10-29 13:19:24 -0300

    added prototype memory buffer access and drop operation

--- parfast.lua
+++ parfast.lua
@@ -28,8 +28,15 @@ Reserved = {
 	SWAP = enum(),
 	WHILE = enum(),
 	DO = enum(),
-	ELSE = enum()
+	ELSE = enum(),
+	MBUF = enum(),
+	LOAD = enum(),
+	STORE = enum(),
+	DROP = enum()
 }
+
+-- probably will be more than this is the future.
+local max_buffer_cap = 1000
 
 local strreserved = {
 	["puts"] = Reserved.PUTS,
@@ -41,7 +48,11 @@ local strreserved = {
 	["swap"] = Reserved.SWAP,
 	["while"] = Reserved.WHILE,
 	["do"] = Reserved.DO,
-	["else"] = Reserved.ELSE
+	["else"] = Reserved.ELSE,
+	["mbuf"] = Reserved.MBUF,
+	["st"] = Reserved.STORE,
+	["ld"] = Reserved.LOAD,
+	["drop"] = Reserved.DROP
 }
 
 local function push(val)
@@ -87,7 +98,19 @@ local function _do()
 	return { Reserved.DO }
 end
 local function _else()
-	return {Reserved.ELSE}
+	return { Reserved.ELSE }
+end
+local function st()
+	return { Reserved.STORE }
+end
+local function ld()
+	return { Reserved.LOAD }
+end
+local function mbuf()
+	return { Reserved.MBUF }
+end
+local function drop()
+	return { Reserved.DROP }
 end
 
 local function lexl(line)
@@ -98,6 +121,7 @@ local function lexl(line)
 	for i = 1, string.len(line) do
 		table.insert(src, line:sub(i, i))
 	end
+
 
 	i = 1
 	local function shift()
@@ -203,60 +227,93 @@ function parse(tokens)
 
 	while #tokens > 0 do
 		if tokens[1].value == "puts" then
-			assert(#program > 0, string.format("Cannot write as the stack may be empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 0,
+				string.format("Cannot write as the stack may be empty. At %d:%d", tokens[1].line, tokens[1].col))
 			shift()
 			table.insert(program, puts())
 		elseif tokens[1].value == "+" then
-			assert(#program > 1, string.format("Cannot make arithmetic(+) as the stack may be empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 1,
+				string.format("Cannot make arithmetic(+) as the stack may be empty. At %d:%d", tokens[1].line,
+					tokens[1].col))
 			shift()
 			table.insert(program, add())
 		elseif tokens[1].value == "-" then
-			assert(#program > 1, string.format("Cannot make arithmetic(-) as the stack may be empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 1,
+				string.format("Cannot make arithmetic(-) as the stack may be empty. At %d:%d", tokens[1].line,
+					tokens[1].col))
 			shift()
 			table.insert(program, sub())
 		elseif tokens[1].value == "if" then
-			assert(#program > 2, string.format("Expected condition before `if` initial block. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 2,
+				string.format("Expected condition before `if` initial block. At %d:%d", tokens[1].line, tokens[1].col))
 			shift()
 			table.insert(program, _if())
 		elseif tokens[1].value == "end" then
 			shift()
 			table.insert(program, _end())
 		elseif tokens[1].value == "==" then
-			assert(#program > 1, string.format("Cannot make boolean operation(==) as the stack is empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 1,
+				string.format("Cannot make boolean operation(==) as the stack is empty. At %d:%d", tokens[1].line,
+					tokens[1].col))
 			shift()
 			table.insert(program, equ())
 		elseif tokens[1].value == "!=" then
-			assert(#program > 1, string.format("Cannot make boolean operation(!=) as the stack is empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 1,
+				string.format("Cannot make boolean operation(!=) as the stack is empty. At %d:%d", tokens[1].line,
+					tokens[1].col))
 			shift()
 			table.insert(program, neq())
 		elseif tokens[1].value == ">" then
-			assert(#program > 1, string.format("Cannot make boolean operation(>) as the stack is empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 1,
+				string.format("Cannot make boolean operation(>) as the stack is empty. At %d:%d", tokens[1].line,
+					tokens[1].col))
 			shift()
 			table.insert(program, gt())
 		elseif tokens[1].value == "<" then
-			assert(#program > 1, string.format("Cannot make boolean operation(<) as the stack is empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 1,
+				string.format("Cannot make boolean operation(<) as the stack is empty. At %d:%d", tokens[1].line,
+					tokens[1].col))
 			shift()
 			table.insert(program, lt())
 		elseif tokens[1].value == "dup" then
-			assert(#program > 0, string.format("Cannot duplicate the top of the stack as it is empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 0,
+				string.format("Cannot duplicate the top of the stack as it is empty. At %d:%d", tokens[1].line,
+					tokens[1].col))
 			shift()
 			table.insert(program, dup())
 		elseif tokens[1].value == "swap" then
-			assert(#program > 1, string.format("Cannot swap the top of the stack as it is empty. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 1,
+				string.format("Cannot swap the top of the stack as it is empty. At %d:%d", tokens[1].line, tokens[1].col))
 			shift()
 			table.insert(program, swp())
 		elseif tokens[1].value == "while" then
 			shift()
 			table.insert(program, _while())
 		elseif tokens[1].value == "do" then
-			assert(#program > 2, string.format("Expected condition before `do` initial block. At %d:%d", tokens[1].line, tokens[1].col))
+			assert(#program > 2,
+				string.format("Expected condition before `do` initial block. At %d:%d", tokens[1].line, tokens[1].col))
 			shift()
 			table.insert(program, _do())
 		elseif tokens[1].value == "else" then
 			shift()
 			table.insert(program, _else())
+		elseif tokens[1].value == "st" then
+			shift()
+			table.insert(program, st())
+		elseif tokens[1].value == "ld" then
+			shift()
+			table.insert(program, ld())
+		elseif tokens[1].value == "mbuf" then
+			shift()
+			table.insert(program, mbuf())
+		elseif tokens[1].value == "drop" then
+			shift()
+			table.insert(program, drop())
 		else
-			assert(#tokens > 1, string.format("Warn: the result of the push operation at eof will be considered as dead code.\n\tAt location %d:%d", tokens[1].line, tokens[1].col))
+			assert(#tokens > 1,
+				string.format(
+					"Warn: the result of the push operation at eof will be considered as dead code.\n\tAt location %d:%d",
+					tokens[1].line, tokens[1].col))
 			local val = shift().value
 			table.insert(program, push(val))
 		end
@@ -292,7 +349,7 @@ local function get_references(program)
 				print("Else is not being used in an if statement.")
 				os.exit(1)
 			end
-			program[if_location] = {Reserved.IF, i + 1}
+			program[if_location] = { Reserved.IF, i + 1 }
 			table.insert(ref_stack, i)
 		elseif opr[1] == Reserved.DO then
 			local while_ref = table.remove(ref_stack)
@@ -305,7 +362,7 @@ local function get_references(program)
 end
 
 function compile(ir, outname)
-	local output = io.open(outname..".asm", "w+")
+	local output = io.open(outname .. ".asm", "w+")
 	if not output or output == nil then
 		return nil
 	end
@@ -315,6 +372,7 @@ function compile(ir, outname)
 	output:write(
 		".L2:\n\tmov  rax, rdi\n\tlea  r8, [rsp+32]\n\tmul  r9\n\tmov  rax, rdi\n\tsub  r8, rcx\n\tshr  rdx, 3\n\tlea  rsi, [rdx+rdx*4]\n\tadd  rsi, rsi\n\tsub  rax, rsi\n\tadd  eax, 48\n\tmov  BYTE [rcx], al\n\tmov  rax, rdi\n\tmov  rdi, rdx\n\tmov  rdx, rcx\n\tsub  rcx, 1\n\tcmp  rax, 9\n\tja   .L2\n\tlea  rax, [rsp+32]\n\tmov  edi, 1\n\tsub  rdx, rax\n\tlea  rsi, [rsp+32+rdx]\n\tmov  rdx, r8\n\tmov  rax, 1\n\tsyscall\n\tadd  rsp, 40\n\tret\n")
 
+	output:write("section .bss\n\tmbuf: resb " .. max_buffer_cap .. "\n")
 	output:write("section .text\n\tglobal _start\n\n_start:\n")
 
 	for i, op in pairs(ir) do
@@ -337,13 +395,17 @@ function compile(ir, outname)
 		elseif op[1] == Reserved.DUP then
 			output:write("\tpop rax\n\tpush rax\n\tpush rax\n")
 		elseif op[1] == Reserved.EQU then
-			output:write("\tmov rcx, 0\n\tmov rdx, 1\n\tpop rax\n\tpop rbx\n\tcmp rax, rbx\n\tcmove rcx, rdx\n\tpush rcx\n")
+			output:write(
+			"\tmov rcx, 0\n\tmov rdx, 1\n\tpop rax\n\tpop rbx\n\tcmp rax, rbx\n\tcmove rcx, rdx\n\tpush rcx\n")
 		elseif op[1] == Reserved.NEQ then
-			output:write("\tmov rcx, 1\n\tmov rdx, 0\n\tpop rax\n\tpop rbx\n\tcmp rax, rbx\n\tcmove rcx, rdx\n\tpush rcx\n")
+			output:write(
+			"\tmov rcx, 1\n\tmov rdx, 0\n\tpop rax\n\tpop rbx\n\tcmp rax, rbx\n\tcmove rcx, rdx\n\tpush rcx\n")
 		elseif op[1] == Reserved.GT then
-			output:write("\tmov rcx, 0\n\tmov rdx, 1\n\tpop rbx\n\tpop rax\n\tcmp rax, rbx\n\tcmovg rcx, rdx\n\tpush rcx\n")
+			output:write(
+			"\tmov rcx, 0\n\tmov rdx, 1\n\tpop rbx\n\tpop rax\n\tcmp rax, rbx\n\tcmovg rcx, rdx\n\tpush rcx\n")
 		elseif op[1] == Reserved.LT then
-			output:write("\tmov rcx, 0\n\tmov rdx, 1\n\tpop rbx\n\tpop rax\n\tcmp rax, rbx\n\tcmovg rcx, rdx\n\tpush rcx\n")
+			output:write(
+			"\tmov rcx, 0\n\tmov rdx, 1\n\tpop rbx\n\tpop rax\n\tcmp rax, rbx\n\tcmovl rcx, rdx\n\tpush rcx\n")
 		elseif op[1] == Reserved.SWAP then
 			output:write("\tpop rax\n\tpop rbx\n\tpush rax\n\tpush rbx\n")
 		elseif op[1] == Reserved.WHILE then
@@ -352,9 +414,17 @@ function compile(ir, outname)
 			output:write(string.format("\tpop rax\n\ttest rax, rax\n\tjz op_%d\n", op[2]))
 		elseif op[1] == Reserved.ELSE then
 			output:write(string.format("\tjmp op_%d\n", op[2]))
+		elseif op[1] == Reserved.MBUF then
+			output:write("\tpush mbuf\n")
+		elseif op[1] == Reserved.LOAD then
+			output:write("\tpop rax\n\txor rbx, rbx\n\tmov bl, [rax]\n\tpush rbx\n")
+		elseif op[1] == Reserved.STORE then
+			output:write("\tpop rbx\n\tpop rax\n\tmov [rax], bl\n")
+		elseif op[1] == Reserved.DROP then
+			output:write("\tpop rax\n")
 		else
-       print("\27[31;4mError\27[0m:\n\tOperand not recognized")
-       os.exit(1)
+			print("\27[31;4mError\27[0m:\n\tOperand not recognized ")
+			os.exit(1)
 		end
 	end
 
@@ -363,8 +433,10 @@ function compile(ir, outname)
 	output:close()
 end
 
+-- Extension should be .parfast
 local function getfilename(filepath)
-  return filepath:sub(1, -9)
+	local sfilepath = string.gsub(filepath, ".parfast", "")
+	return sfilepath
 end
 
 function main()
@@ -375,12 +447,12 @@ function main()
 	end
 	local tokens = lexl(input:read("a"))
 	local ir = parse(tokens)
-  local outname = getfilename(arg[1])
+	local outname = getfilename(arg[1])
 	compile(get_references(ir), outname)
-  os.execute("nasm -f elf64 "..outname..".asm")
-	os.execute(string.format("ld -o %s %s", outname, outname..".o"))
+	os.execute("nasm -f elf64 " .. outname .. ".asm")
+	os.execute(string.format("ld -o %s %s", outname, outname .. ".o"))
 
-	print("commands: \n\t[nasm -f elf64 a.asm]\n\t[ld -o a.out a.o]")
+	print(string.format("Commands: \n\t[nasm -f elf64 %s.asm]\n\t[ld -o %s %s.o]", outname, outname, outname))
 end
 
 main()
